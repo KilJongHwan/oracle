83b005a84b2a34203277b8ad55694b761a0321ca
JH <dafr47@naver.com>
2023-09-18 12:49:15 +0900

    조인 추가

--- GROUP.sql
+++ GROUP.sql
@@ -121,3 +121,57 @@ ORDER BY DEPTNO, JOB;
 SELECT DEPTNO, JOB, COUNT(*), MAX(SAL), SUM(SAL), AVG(SAL)
 FROM EMP
 GROUP BY ROLLUP(DEPTNO, JOB);
+
+-- JOIN : 두 개 이상의 테이블에서 데이터를 가져와서 연결하는 데 사용하는 SQL 기능
+-- RDMS에서는 테이블 설계 시 무결성 원칙으로 인해 동일한 정보가 여러 군데 존재하면 안되기 때문에
+-- 필연적으로 테이블을 관리 목적에 맞게 설계
+SELECT * FROM DEPT;
+
+-- 열 이름을 비교하는 조건식으로 조인하기
+SELECT *
+FROM EMP , DEPT
+WHERE EMP.DEPTNO = DEPT.DEPTNO
+ORDER BY EMPNO;
+
+-- 테이블 별칭 사용하기
+SELECT *
+FROM EMP E, DEPT D
+    WHERE E.DEPTNO = D.DEPTNO
+ORDER BY EMPNO;
+
+-- 조인 종류 : 두 개 이상의 테이블을 하나의 테이블처럼 가로로 늘려서 출력하기 위해 사용
+-- 조인은 대상 데이터를 어떻게 연결하느냐에 따라 등가, 비등가, 자체, 외부 조인으로 구분
+-- 등가 조인 : 테이블을 연결한 후 출력 행을 각 테이블 특정 열에 일치한 데이터를 기준으로 선정하는 방법
+-- 등가 조인에는 ANSI(안시) 조인과 오라클 조인이 있음
+SELECT EMPNO, ENAME, D.DEPTNO, DNAME, LOC
+    FROM EMP E, DEPT D
+    WHERE E.DEPTNO = D.DEPTNO
+    AND E.DEPTNO = 10
+    ORDER BY D.DEPTNO;
+
+SELECT EMPNO, ENAME, SAL, D.DEPTNO, DNAME, LOC
+    FROM EMP E, DEPT D
+    WHERE E.DEPTNO = D.DEPTNO
+    AND SAL >= 3000
+    ORDER BY D.DEPTNO;
+
+SELECT EMPNO, ENAME, SAL, D.DEPTNO, DNAME ,LOC 
+    FROM EMP E JOIN DEPT D
+    ON E.DEPTNO = D.DEPTNO
+    WHERE SAL >= 3000
+    ORDER BY D.DEPTNO;
+
+-- EMP 테이블 별칭을 E , DEPT 테이블 별칭을 D로 하여 다음과 같이 등가 조인을 했을 때
+-- 급여가 2500 이하이고 사원번호가 9999 이하인 사원의 정보가 출력 되도록 작성(ANSI, Oracle 조인)
+-- ANSI 조인
+SELECT *
+    FROM EMP E JOIN DEPT D
+    ON E.DEPTNO = D.DEPTNO  -- 동등 조인, 이너 조인(두 테이블이 일치하는 데이터만 선택)
+    WHERE SAL <= 2500 AND EMPNO <= 9999
+    ORDER BY EMPNO;
+-- Oracle 조인
+SELECT *
+    FROm EMP E, DEPT D
+    WHERE E.DEPTNO = D.DEPTNO
+    AND SAL <= 2500 AND EMPNO <= 9999
+    ORDER BY EMPNO;
